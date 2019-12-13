9aa889e47ee7dbf1f43951ef6c4f1af37e995776
zoemcl <zoemcl@LAPTOP-1BLD0C7U.localdomain>
2019-12-12 20:01:38 -0500

    finished hyperparameter tuning for MR

--- app/simple.py
+++ app/simple.py
@@ -17,19 +17,18 @@ import numpy as np
 
 # load data
 texts, labels_index, labels = read_input_data("../data");
-X_train, X_test, y_train, y_test = train_test_split(texts, labels, test_size=0.2)
-
-print(texts);
+X_train, X_test, y_train, y_test = train_test_split(texts, labels, test_size=0.2);
 
 tfidf_vec = TfidfVectorizer(stop_words="english", ngram_range=(1,2), norm='l2'); #token_pattern=r'\b\w+\b'
 
 text_clf = Pipeline([ 
     ('tfvec', tfidf_vec),
     #('clf', LinearSVC(0.9))
-    ('clf', KNeighborsClassifier(n_neighbors=7))
-    #('clf', MultinomialNB()),
-    #('clf', LogisticRegression()),
-    #('clf', RandomForestClassifier()),
+    #('clf', KNeighborsClassifier(n_neighbors=7))
+    #('clf', MultinomialNB(alpha=1.8)),
+    #('clf', LogisticRegression(C=3.1, class_weight='balanced')),
+    #('clf', RandomForestClassifier(n_estimators=100, class_weight='balanced_subsample'))
+    ('clf', AdaBoostClassifier(n_estimators=100, )),
 ])
 
 # comment out parmeters that you want to tune, not all of them are here yet
@@ -44,7 +43,18 @@ grid_params = {
     # 'clf__C': (0.5,0.8,1,1.5)
     #### KNeighboursClassifier ####
     #'clf__n_neighbors': (2,3,4,5,6,7,8)
-
+    #### MultiNomialNB() ####
+    #'clf__alpha': (1.7, 1.8, 1.9, 2, 2.1)
+    #### LogisticRegression() ####
+    # 'clf__C': (3, 3.1),
+    # 'clf__solver': ('lbfgs', 'liblinear', 'sag', 'saga'),
+    # 'clf__class_weight': ('balanced', None)
+    #### RandomForestClassifier() ####
+    # 'clf__n_estimators': (50, 100, 150)
+    # 'clf__class_weight': ('balanced', 'balanced_subsample', None)
+    #### AdaBoostClassifier() ####
+    'clf__n_estimators': (30, 50, 100, 130)
+    # 'clf__class_weight': ('balanced', 'balanced_subsample', None)
 }
 
 gsCV = GridSearchCV(text_clf, grid_params, cv=5, verbose=5);
